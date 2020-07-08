377ee26938c9d85f0efb06cfcaa8cf4476e5f967
abiliobrio <abiliobrio@gmail.com>
2020-07-08 14:56:53 -0300

    Versão inicial (start.spring.io)

---
+++ desafio-java-9/src/test/java/com/challenge/SpringChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
