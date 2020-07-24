56876001a5bb1a1822bc87a3a767378521d40c6e
abiliobrio <abiliobrio@gmail.com>
2020-07-23 22:21:04 -0300

    Iniciando

---
+++ desafio-java-10/src/test/java/com/challenge/SpringChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
